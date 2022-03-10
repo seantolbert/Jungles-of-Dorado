e49b8836766d714b8ee2ecb61bfb92393028c9d7
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-03-10 13:37:10 -0500

    makes secure

--- blogoroddyv3/settings/production.py
+++ blogoroddyv3/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
 
 DEBUG = False
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+PREPEND_WWW = True
+SESSION_COOKIE_SECURE = True
 
 try:
     from .local import *
