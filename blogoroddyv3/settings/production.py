2eaae7a58c8081fa111508641f95c74aee48f44d
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-03-11 13:43:01 -0500

    ??

--- blogoroddyv3/settings/production.py
+++ blogoroddyv3/settings/production.py
@@ -1,11 +1,12 @@
 from .base import *
 from decouple import config
 
-DEBUG = False
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 PREPEND_WWW = True
 SESSION_COOKIE_SECURE = True
+DEBUG = False
 
 try:
     from .local import *
