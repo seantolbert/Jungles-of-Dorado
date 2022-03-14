f4d35ba3a25af7c58621b179a416a3e9746dcfc1
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-03-14 12:54:03 -0400

    configures s3bucket

--- blogoroddyv3/settings/base.py
+++ blogoroddyv3/settings/base.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'about',
     'gallery',
 
+    'storages',
+
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -178,3 +180,8 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % config('AWS_STORAGE_BUCKET_NAME')
--- blogoroddyv3/settings/production.py
+++ blogoroddyv3/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_CONTENT_TYPE_NOSNIFF = True
@@ -8,6 +9,8 @@ SESSION_COOKIE_SECURE = True
 DEBUG = False
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['www.junglesofdorado.com']
+MEDIA_URL = "https://%s/" % '%s.s3.amazonaws.com' % config('AWS_STORAGE_BUCKET_NAME')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
     from .local import *
