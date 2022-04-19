47d32d20dd3f21f18e943079dac59fe0bad53cdb
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-04-19 17:00:15 -0400

    stopping for the day

--- blog/models.py
+++ blog/models.py
@@ -8,16 +8,22 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
-# class ImageBlock(blocks.StructBlock):
-#     image = ImageChooserBlock()  
-#     caption = blocks.CharBlock(classname="stream__img-caption", required=False)
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
 
-#     class Meta:
-#         icon = "image/picture"
-#         admin_text = mark_safe("<b>Image Block</b>")
-#         label = "Image Block"
+from taggit.models import TaggedItemBase
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE,
+    )
 
 class BlogPage(Page):
+
+    template = 'blog/blog_page.html'
+
     date = models.DateField(default=timezone.now, blank=True, null=True)
 
     description = models.CharField(max_length=255, blank=True, null=True)
@@ -27,6 +33,8 @@ class BlogPage(Page):
     )
 
     main_image_excerpt = models.CharField(max_length=300, blank=True, null=True)
+
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     body = StreamField(
         [
@@ -45,14 +53,25 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("date"),
         FieldPanel("description"),
+        FieldPanel("tags"),
         ImageChooserPanel("main_image"),
         FieldPanel("main_image_excerpt"),
         StreamFieldPanel("body"),
     ]
 
 
+
+
 class BlogIndexPage(Page):
     def blogs(self):
-        blogs = BlogPage.objects.live()
-        blogs = blogs.order_by("-date")
+        blogs = BlogPage.objects.live().order_by('-date')
         return blogs
+    
+    # def get_context(self, request, *args, **kwargs):
+    #     context = super().get_context(request, *args, **kwargs)
+    #     all_posts = BlogPage.objects.live().order_by("-date")
+    #     # if request.GET.get('tags', None):
+    #     #     tags = request.GET.get('tags')
+    #     #     all_posts = all_posts.filter(tags__slug__in=[tags])
+    #     context["blogs"] = all_posts
+    #     return context
