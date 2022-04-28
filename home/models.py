5ebed5d0bc9154ffd40b743f2aca17d83cb7fb9a
seantolbert <stolbert4150@gmail.com>
2022-04-28 11:27:13 -0400

    added headline to home page

--- home/models.py
+++ home/models.py
@@ -9,12 +9,13 @@ from gallery.models import GalleryPage
 
 class HomePage(Page):
     headline = models.CharField(max_length=100, blank=True, null=True)
-
+    sub_headline = models.CharField(max_length=200, blank=True, null=True)
     content_panels = Page.content_panels + [
         FieldPanel("headline"),
-    ]
+        FieldPanel("sub_headline"),
+    ]   
 
     def blogs(self):
         blogs = BlogPage.objects.live()
-        blogs = blogs.order_by("-date")[:3]
+        blogs = blogs.order_by("-date")[:6]
         return blogs
