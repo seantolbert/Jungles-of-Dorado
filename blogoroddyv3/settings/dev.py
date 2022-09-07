1023e5b9bfb5d95392a908dd2130b221559e5938
seantolbert <stolbert4150@gmail.com>
2022-09-07 17:01:19 -0400

    project_page issue test

--- blogoroddyv3/settings/dev.py
+++ blogoroddyv3/settings/dev.py
@@ -19,6 +19,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASS'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': '5432'
+#     }
+# }
 
 try:
     from .local import *
