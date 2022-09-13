c35b038b9fbe5c6b5411f0b88c9f96be0caf6925
seantolbert <stolbert4150@gmail.com>
2022-09-13 12:09:43 -0400

    adds custom coloring options in custom.css

--- blogoroddyv3/wagtail_hooks.py
+++ blogoroddyv3/wagtail_hooks.py
@@ -1,8 +1,10 @@
 from django.utils.html import format_html
 from django.templatetags.static import static
 
-from wagtail import hooks
+from wagtail.core import hooks
 
-@hooks.register('insert_global_admin_css')
+
+@hooks.register("insert_global_admin_css", order=100)
 def global_admin_css():
-    return format_html('<link rel="stylesheet" href="{}"/>', static('css/blogoroddyv3.css'))
+    # adding
+    return format_html('<link rel="stylesheet" href="{}">', static("css/custom.css"))
