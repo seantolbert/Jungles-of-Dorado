96199b982073575dddb57b84014b7acb37b6b5fc
seantolbert <stolbert4150@gmail.com>
2022-09-26 14:37:00 -0400

    stubbs up double streamfields on either side of the home page so we can manipulate sections

--- home/models.py
+++ home/models.py
@@ -1,12 +1,24 @@
+from email import header
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
+
 
 from blog.models import BlogPage
 
 # from gallery.models import GalleryPage
+
+
+class NewsBlock(blocks.StructBlock):
+    header = blocks.CharBlock()
+    image = ImageChooserBlock(required=False)
+    imageTwo = ImageChooserBlock(required=False)
+    content = blocks.RichTextBlock()
 
 
 class HomePage(Page):
@@ -15,10 +27,35 @@ class HomePage(Page):
     )
     headline = models.CharField(max_length=100, blank=True, null=True)
     sub_headline = models.CharField(max_length=200, blank=True, null=True)
+    
+    above_posts = StreamField(
+        [
+            ("stream_heading", blocks.CharBlock()),
+            ("stream_image", ImageChooserBlock(required=False)),
+            ("stream_image2", ImageChooserBlock(required=False)),
+            ("content", blocks.RichTextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    below_posts = StreamField(
+        [
+            ("stream_heading", blocks.CharBlock()),
+            ("stream_image", ImageChooserBlock(required=False)),
+            ("stream_image2", ImageChooserBlock(required=False)),
+            ("content", blocks.RichTextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
     content_panels = Page.content_panels + [
         ImageChooserPanel("background_img"),
         FieldPanel("headline"),
         FieldPanel("sub_headline"),
+        StreamFieldPanel("above_posts"),
+        StreamFieldPanel("below_posts"),
     ]
 
     def blogs(self):
