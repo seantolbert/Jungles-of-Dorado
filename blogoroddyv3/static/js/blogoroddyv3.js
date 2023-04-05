3596e3a713dc4368d135f4f877b67d099a19cc63
seantolbert <stolbert4150@gmail.com>
2023-04-05 10:54:22 -0400

    changes some navbar formatting

--- blogoroddyv3/static/js/blogoroddyv3.js
+++ blogoroddyv3/static/js/blogoroddyv3.js
@@ -1,15 +1,1 @@
-const modal = document.getElementById("modal");
-
-htmx.on("htmx:afterSwap", (e) => {
-  if (e.detail.target.id == "dialog") {
-    modal.show();
-  }
-});
-
-htmx.on("htmx:beforeSwap", (e) => {
-  if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
-    modal.hide();
-    e.detail.shouldSwap = false;
-  }
-});
-
+const navList = document.getElementsByClassName("navList");
