410ca845d7cf3e3a70379146b01b1d590d5c9040
seantolbert <stolbert4150@gmail.com>
2023-04-06 13:05:41 -0400

    compleets modal capabilities

--- blogoroddyv3/static/js/blogoroddyv3.js
+++ blogoroddyv3/static/js/blogoroddyv3.js
@@ -24,16 +24,40 @@ function menuClose() {
 }
 
 //
-// gallery post
+// modal functionality
 //
 
+const modal = document.querySelector(".modal");
 const modalImg = document.querySelector(".modal-img img");
+const dimmer = document.querySelector(".dimmer");
+const closeBtn = document.querySelector(".close-modal");
 
 let selectedImg = "";
+let selectedLink = "";
 
 function highlight() {
   window.onclick = (e) => {
-    selectedImg = `${e.target.getAttribute("src")}`;
-    modalImg.setAttribute("src", selectedImg);
+    selectedImg = e.target.getAttribute("src");
+    selectedLink = e.target.getAttribute("alt");
+    if (selectedImg !== null) {
+      modalImg.setAttribute("src", selectedImg);
+    }
   };
+  dimmer.style.background = "rgba(0, 0, 0, 0.75)";
+  dimmer.style.pointerEvents = "auto";
+  closeBtn.style.display = "block";
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  dimmer.style.background = "transparent";
+  dimmer.style.pointerEvents = "none";
+  closeBtn.style.display = "none";
+  modal.style.display = "none";
+}
+
+function instaNavigation() {
+  if (selectedLink !== "#" && selectedLink !== "") {
+    window.open(selectedLink, "_blank");
+  }
 }
